7c046d6c7b0e7b8d3194d357d871d1f30a4eb2c9
Chris Jimison <cjimison@ngmoco.com>
2013-04-25 12:49:32 -0700

    removed the cleanup of the tar file

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -5,5 +5,5 @@ git clone https://github.com/cjimison/barrage_build.git
 mv barrage_build/barrage_linux_x64.tgz ./
 rm -rf barrage_build 
 tar -xzvf barrage_linux_x64.tgz
-rm barrage_linux_x64.tgz
+#rm barrage_linux_x64.tgz
 
