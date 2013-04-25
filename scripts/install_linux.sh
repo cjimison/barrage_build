93e494899dcb75e698ad548f0d092e1ca4fb6902
Chris Jimison <cjimison@ngmoco.com>
2013-04-25 12:52:44 -0700

    Fixed the untar command

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -4,6 +4,6 @@ echo Downloading Barrage
 git clone https://github.com/cjimison/barrage_build.git
 mv barrage_build/barrage_linux_x64.tgz ./
 rm -rf barrage_build 
-tar -xzvf barrage_linux_x64.tgz
-#rm barrage_linux_x64.tgz
+tar -xvf barrage_linux_x64.tgz
+rm barrage_linux_x64.tgz
 
